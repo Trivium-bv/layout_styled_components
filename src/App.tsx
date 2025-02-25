084dfaf9bc8c428a5707d13e06b49731d114f4c9
Trivium-bv <trivium.bv@gmail.com>
2025-02-25 14:44:35 +0300

    styled component (example)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
-function App() {
+import styled from 'styled-components';
 
+function App() {
     return (
         <>
-            hello
+            <Box>
+                <StyledBtn>Hello</StyledBtn>
+            </Box>
         </>
     )
 }
-
 export default App
+
+const StyledBtn = styled.button`
+    border: none;
+    background-color: #fa5879;
+    padding: 10px 20px;
+    border-radius: 5px;
+    color: #f8dede;
+    font-size: 2rem;
+    font-weight: bold;
+`
+
+
+const Box = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
