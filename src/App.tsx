bc661ef0a31bfb7d01cb2c42b436f8b2de089263
Trivium-bv <trivium.bv@gmail.com>
2025-02-25 16:55:10 +0300

    case with as

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
     return (
         <>
             <Box>
+                <StyledBtn as={Link} href={"#"}>LinkComponent</StyledBtn>
+                <StyledBtn as="a" href={"#"}>Link</StyledBtn>
                 <StyledBtn>Hello</StyledBtn>
                 <SuperButton>SuperButton</SuperButton>
             </Box>
@@ -21,16 +23,26 @@ const StyledBtn = styled.button`
     font-size: 2rem;
     font-weight: bold;
 `
-//расширения "extends"
-const SuperButton = styled(StyledBtn)`
+
+const Link = styled.a`
+    color: #6d0cff;
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: #f9f9f9;
+    `
+
+    //расширения "extends"
+    const SuperButton = styled(StyledBtn)`
     border-radius: 5px;
     background-color: #ffed7b;
     color: #064cff;
 `
 
 const Box = styled.div`
+    min-width: 1000px;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 20px;
 `
